07db6048a007766fafeaefe5850275939d8564ec
Henning Schmiedehausen <henning@schmiedehausen.org>
2014-12-28 21:29:36 -0800

    add integration test for resource conflict between main and test classes folder

---
+++ src/it/test-resource-conflict-local/verify.groovy
@@ -0,0 +1,22 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import static org.basepom.mojo.duplicatefinder.groovy.ITools.*
+
+def result = loadTestXml(basedir)
+
+overallState(CONFLICT_DIFF, 1, FAILED, result) // This is "1" because both conflictResults are in the same conflict.
+checkConflictResult("conflict-same-content",      TYPE_RESOURCE, CONFLICT_EQUAL, NOT_EXCEPTED, PRINTED, FAILED, findConflictResult(result, 2, projectTargetFolder(basedir), projectTargetTestFolder(basedir)))
+checkConflictResult("conflict-different-content", TYPE_RESOURCE, CONFLICT_DIFF,  NOT_EXCEPTED, PRINTED, FAILED, findConflictResult(result, 2, projectTargetFolder(basedir), projectTargetTestFolder(basedir)))
+
+return true
